bd76378ed03388809b81a8b19aeecdf8f224f6fd
Shruti1602003 <111267511+Shruti1602003@users.noreply.github.com>
2024-01-23 15:05:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`https://ecom-api-steel.vercel.app/images/${req.file.filename}`
     })
 })
 
